93c45cf8aa168fb1553d27650fcad9d4e31a9aee
Preston Mitchell <40524048+SolemnReaper@users.noreply.github.com>
2020-07-05 00:51:36 -0500

    Remove Token

--- src/main/java/com/github/preston/Main.java
+++ src/main/java/com/github/preston/Main.java
@@ -6,7 +6,7 @@ import org.javacord.api.DiscordApiBuilder;
 public class Main {
 
     public static void main(String[] args) {
-        DiscordApi api = new DiscordApiBuilder().setToken("NzI5MjA2OTA4NDY0MDA1MTUw.XwFl-Q.Ry4p4DaFCHHfCA0mvmca7ujGLSw").login().join();
+        DiscordApi api = new DiscordApiBuilder().setToken("XXXXXX").login().join();
         System.out.println("Logged in!");
     }
 
